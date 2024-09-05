2b699fa22042a5bb0be799b7ea94e639d5f48bc7
SenseiMori <mtpchelokot@gmail.com>
2024-09-05 11:56:47 +0500

    Добавьте файлы проекта.

---
+++ ConsoleGames/Code/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics.Contracts;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   
+            
+           //Вызов метода из класса
+            Snake.Game();
+
+
+
+
+
+
+
+        }
+        
+    }
+}
+
